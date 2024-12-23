5f357002bf946780fd708456f60e60719366c591
Txus <jsus.90@gmail.com>
2024-12-23 01:35:09 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,7 @@
-# Download vim-plug
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim \
   --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim' \
 
-# Install minvim
 sh -c 'curl -o ~/.config/nvim/init.lua --create-dirs \
     https://raw.githubusercontent.com/jelohe/minvim/refs/heads/main/init.lua'
 
-# Open nvim and install plugins
 nvim +PlugInstall +qa
